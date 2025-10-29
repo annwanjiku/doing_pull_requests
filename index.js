0f4741ced0cfed96767563039ce574e6fef7aafa
annwanjiku <wanjikunganga21@gmail.com>
2025-10-29 10:11:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const values = ['ONE','TWO','THREE','FOUR','FIVE'];
+const button = document.getElementById("button_one");
+let index=0;
+
+button.addEventListener("click",()=>{
+    button.innerHTML=values[index];
+    index=index+1;
+    if(index>=values.length){
+        index=0;
+    }
+})
